0312bb5ca968ea4e7df3c585fde7663d6f3d2307
ChangWoongHong <ghdcksdnd11@gmail.com>
2025-07-25 17:47:46 +0900

    서브모듈 변경 사항 커밋

--- app.py
+++ app.py
@@ -6,6 +6,12 @@ app = Flask(__name__)
 def home():
     return render_template("index.html") # index.html 파일 렌더링
 
+
+@app.route('/버튼_클릭_동작')
+def button_action():
+    return "버튼이 클릭됐습니다!" # 버튼 클릭 시 이 문구 출력
+
+
 if __name__ == '__main__':
     app.run(debug=True)
 
